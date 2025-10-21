87ab4e4402c6a885031d0c9293c2fa3677f08367
TheMajoris <74721713+TheMajoris@users.noreply.github.com>
2025-10-21 12:22:00 +0800

    Implemented defensive check for usernames

--- components/ui/UserDropdown.tsx
+++ components/ui/UserDropdown.tsx
@@ -17,7 +17,7 @@ import NavItems from "./NavItems";
 const UserDropdown = () => {
     const router = useRouter();
 
-    const handleSignOut= async() => {
+    const handleSignOut= async () => {
         router.push("/sign-in");
     }
 
@@ -30,7 +30,7 @@ const UserDropdown = () => {
                 <Avatar className="h-10 w-10">
                     <AvatarImage src="https://github.com/shadcn.png" />
                     <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
-                        {user.name[0]}
+                        {user.name[0] || '?'}
                     </AvatarFallback>
                 </Avatar>
                 <div className="hidden md:flex flex-col items-start">
@@ -56,7 +56,7 @@ const UserDropdown = () => {
             </DropdownMenuLabel>
             <DropdownMenuSeparator className="bg-gray-600" />
             <DropdownMenuItem onClick={handleSignOut} className="text-gray-100 text-md font-medium focus:bg-transparent focus:text-yellow-500 cursor-pointer">
-                <LogOut className="h--4 w-4 mr-2 hidden sm:block"/> 
+                <LogOut className="h-4 w-4 mr-2 hidden sm:block"/> 
                 Logout
             </DropdownMenuItem>
             <DropdownMenuSeparator className="bg-gray-600" />
