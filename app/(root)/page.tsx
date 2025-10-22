8a46811c45befe27fbce2e40133b8adf2c509dc8
TheMajoris <74721713+TheMajoris@users.noreply.github.com>
2025-10-22 23:03:32 +0800

    Added a stock heatmap to the main page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -18,7 +18,15 @@ const Home = () => {
                     height={600}
                 />
             </div>
-
+            <div className="md-col-span xl:col-span-2">
+                <TradingViewWidget
+                    title="Stock Heatmap"
+                    scriptUrl="https://s3.tradingview.com/external-embedding/embed-widget-stock-heatmap.js"
+                    config={MARKET_OVERVIEW_WIDGET_CONFIG}
+                    className="custom-chart"
+                    height={600}
+                />
+            </div>
         </section>
     </div>
   )
