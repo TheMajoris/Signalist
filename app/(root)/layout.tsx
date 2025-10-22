d59e791b1ad9763862ec71c24865e5cc7a4d2f4c
TheMajoris <74721713+TheMajoris@users.noreply.github.com>
2025-10-22 12:23:20 +0800

    Refactored UI components into correct folders

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from '@/components/ui/Header'
+import Header from '@/components/Header'
 
 const layout = ({ children }: {children : React.ReactNode }) => {
   return (
--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -10,7 +10,7 @@ import {
 } from "@/components/ui/dropdown-menu"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { useRouter } from "next/navigation"
-import { Button } from "./button";
+import { Button } from "./ui/button";
 import { LogOut } from "lucide-react";
 import NavItems from "./NavItems";
 
