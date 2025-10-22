e3d09abe1e51966acb1de90bb6b02e17718c4224
TheMajoris <74721713+TheMajoris@users.noreply.github.com>
2025-10-22 23:22:10 +0800

    Readjusted the sections for the

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -26,22 +26,24 @@ const Home = () => {
                     height={600}
                 />
             </div>
-            <div className="md-col-span">
-                <TradingViewWidget
-                    scriptUrl={`${scriptUrl}timeline.js`}
-                    config={TOP_STORIES_WIDGET_CONFIG}
-                    className="custom-chart"
-                    height={600}
-                />
-            </div>
-            <div className="md-col-span xl:col-span-2">
-                <TradingViewWidget
-                    scriptUrl={`${scriptUrl}market-quotes.js`}
-                    config={MARKET_DATA_WIDGET_CONFIG}
-                    height={600}
-                />
-            </div>
         </section>
+            <section className="grid w-full gap-8 home-section">
+                <div className="md-col-span xl:col-span-1">
+                    <TradingViewWidget
+                        scriptUrl={`${scriptUrl}timeline.js`}
+                        config={TOP_STORIES_WIDGET_CONFIG}
+                        className="custom-chart"
+                        height={600}
+                    />
+                </div>
+                <div className="md-col-span xl:col-span-2">
+                    <TradingViewWidget
+                        scriptUrl={`${scriptUrl}market-quotes.js`}
+                        config={MARKET_DATA_WIDGET_CONFIG}
+                        height={600}
+                    />
+                </div>
+            </section>
     </div>
   )
 }
