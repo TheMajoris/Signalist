dc784f1844be6f6018d478d581390a7df2e7c430
TheMajoris <74721713+TheMajoris@users.noreply.github.com>
2025-12-08 20:10:54 +0800

    Implemented InputField component and added Full Name input field.

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import InputField from '@/components/forms/inputField';
 import { Button } from '@/components/ui/button';
 import React from 'react'
 import { useForm, SubmitHandler } from "react-hook-form"
@@ -37,10 +38,17 @@ const SignUp = () => {
             <h1 className="form-title">Sign Up & Personalise</h1>
 
             <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
-                {/*Input fields */}
+                <InputField 
+                    name="fullName"
+                    label="Full Name"
+                    placeholder="John Doe"
+                    register={register}
+                    error={errors.fullName}
+                    validation={{ required: 'Full Name is Required', minLength: 2}}
+                />
 
                 <Button type="submit" disabled={isSubmitting} className="yellow-btn w-full mt-5">
-                    {isSubmitting ? 'Creating account' : 'Start Your Investing Journey'}
+                    { isSubmitting ? 'Creating account' : 'Start Your Investing Journey' }
                 </Button>
             </form>
         </>
--- components/forms/inputField.tsx
+++ components/forms/inputField.tsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import { Label } from '../ui/label'
+import { cn } from '@/lib/utils'
+import { Input } from '../ui/input'
 
-const inputField = () => {
-  return (
-    <div>inputField</div>
-  )
+interface FormInputProps {
+    name: string
+    label: string
+    placeholder?: string
+    type?: string
+    register: any
+    error?: { message?: string }
+    validation?: any
+    disabled?: boolean
+    value?: string | number
+}
+
+const inputField = ({ name, label, placeholder, type="text", register, error, validation, disabled, value }: FormInputProps) => {
+    return (
+        <div className="space-y-2">
+            <Label htmlFor={name} className="form-label">
+                {label}
+            </Label>
+            <Input 
+                type={type}
+                id={name}
+                placeholder={placeholder}
+                disabled={disabled}
+                value={value}
+                className={cn('form-input', { 'opacity-50 cursor-not-allowed': disabled })}
+                {...register(name, validation)}
+            />
+            {error && <p className="text-sm text-red-500">{error.message}</p>}
+        </div>
+    )
 }
 
 export default inputField
